488a0c0373d08a58952e7a184f7d2be9b05da618
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-08-20 13:38:43 +0530

    Game

---
+++ Game.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Board } from "./TicTacToe";
+
+import "./TicTacToe.css";
+
+const Game = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
+
+export default Game;
